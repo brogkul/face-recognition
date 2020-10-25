b6b3f8d9ddb0e2b542871304ae415d5cd9e5d36f
Anthony Melean <brogkul@gmail.com>
2020-10-24 21:06:57 -0400

    Fixing endpoints

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://yourURL/register', {
+    fetch('https://cryptic-lake-63088.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
